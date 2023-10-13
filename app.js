bb1a6b5def218ae282aefc0e640c32d3994a907a
Sergey Kuznetsov <sergey@iterudit.com>
2023-10-13 07:47:08 -0400

    Code fixing and refactoring. It is still break the code in some cases but decodes code where depyc fails. More code stabilisation needed, and I will refactor it to create AST trees first

--- app.js
+++ app.js
@@ -3,21 +3,56 @@
 const PycReader = require('./lib/PycReader')
 const PycDecompiler = require('./lib/PycDecompiler');
 const PycDisassembler = require('./lib/PycDisassembler');
+const ZipReader = require('./lib/zip_reader');
+const zlib = require('zlib');
 const fs = require('fs');
 const path = require('path');
 
-function DecompileModule(filename)
-{
+let g_baseDir = './decompiled/';
+let g_cliArgs = {
+    raw: false,
+    dump: false,
+    asm: false,
+    baseDir: null,
+    filename: []
+};
 
+function parseCLIParams() {
+    for (let idx = 2; idx < process.argv.length; idx++ ) {
+        let cliParam = process.argv[idx];
+        if (cliParam.toLowerCase() == "--asm") {
+            g_cliArgs.asm = true;
+        } else if (cliParam.toLowerCase() == "--raw") {
+            g_cliArgs.raw = true;
+        } else if (cliParam.toLowerCase() == "--dump") {
+            g_cliArgs.dump = true;
+        } else if (cliParam.toLowerCase() == "--basedir") {
+            g_cliArgs.baseDir = process.argv[++idx];
+        } else {
+            g_cliArgs.filename.push(cliParam);
+        }
+    }
+}
+
+function decompilePycObject(data) {
     try
     {
-        let rdr = new PycReader(filename);
+        let rdr = new PycReader(data);
         let obj = rdr.ReadObject();
+        let filename = g_baseDir + obj.FileName;
+        console.log(`Processing ${filename}...`);
+        let dirPath =  path.dirname(filename);
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, {recursive: true});
+        }
         let filenameBase = filename.substring(0, filename.lastIndexOf('.'));
-        if (process.argv.includes('--dump')) {
+        if (g_cliArgs.raw) {
+            fs.writeFileSync(filenameBase + ".pyc", rdr.Reader);
+        }
+        if (g_cliArgs.dump) {
             fs.writeFileSync(filenameBase + ".dump", PycReader.DumpObject(obj));
         }
-        if (process.argv.includes('--asm')) {
+        if (g_cliArgs.asm) {
             fs.writeFileSync(filenameBase + ".pyasm", PycDisassembler.Disassemble(obj));
         }
         fs.writeFileSync(filenameBase + ".py", PycDecompiler.Decompile(obj).toString());
@@ -26,5 +61,27 @@ function DecompileModule(filename)
     {
         console.log(`EXCEPTION: ${ex}`);
     }
+
 }
-DecompileModule(process.argv[2]);
+
+
+function DecompileModule(filenames)
+{
+    for (let filename of filenames) {
+        let zipReader = new ZipReader(filename);
+        if (zipReader.isZipFile()) {
+            for (let entry of zipReader.entries()) {
+                let uncompressedData = zlib.inflateSync(entry.data);
+
+                decompilePycObject(uncompressedData);
+            }
+        } else {
+            decompilePycObject(filename);
+        }
+    }
+}
+
+parseCLIParams()
+g_baseDir = (g_cliArgs.baseDir ? g_cliArgs.baseDir : path.dirname(g_cliArgs.filename)) + '/decompiled/';
+
+DecompileModule(g_cliArgs.filename);
